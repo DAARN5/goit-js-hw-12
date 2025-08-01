f61af04aa6121569405e88b4bdfa90f58275c1aa
DAARN5 <163350434+DAARN5@users.noreply.github.com>
2025-08-01 12:34:01 +0300

    hw5

--- src/main.js
+++ src/main.js
@@ -6,13 +6,11 @@ import {
   hideLoader,
   showLoadMoreButton,
   hideLoadMoreButton,
-  refreshLightbox,
   scrollPage
 } from './js/render-functions';
 import { getImagesByQuery } from './js/pixabay-api';
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
-
 
 let currentQuery = '';
 let currentPage = 1;
@@ -22,8 +20,8 @@ let totalHits = 0;
 const form = document.querySelector('.form');
 const input = document.querySelector('input[name="search-text"]');
 const loadMoreBtn = document.querySelector('.load-more');
- hideLoadMoreButton();
 
+hideLoadMoreButton();
 
 form.addEventListener('submit', async e => {
   e.preventDefault();
@@ -33,39 +31,40 @@ form.addEventListener('submit', async e => {
     iziToast.warning({ message: 'Please enter a search query.' });
     return;
   }
-  
+
   currentQuery = query;
   currentPage = 1;
   clearGallery();
+  hideLoadMoreButton();
   showLoader();
- 
-
 
   try {
     const data = await getImagesByQuery(currentQuery, currentPage);
     const { hits, totalHits: total } = data;
-    totalHits = total; 
+    totalHits = total;
 
     if (hits.length === 0) {
       iziToast.info({
         message: 'Sorry, there are no images matching your search query. Please try again!',
       });
+      hideLoadMoreButton();
       return;
     }
-  
-    createGallery(hits);
-      if (totalHits > currentPage * perPage) {
+
+    createGallery(hits); 
+
+    if (totalHits > currentPage * perPage) {
       showLoadMoreButton();
+    } else {
+      hideLoadMoreButton();
     }
   } catch (error) {
     iziToast.error({ message: 'Something went wrong. Please try again later.' });
+    hideLoadMoreButton();
   } finally {
     hideLoader();
   }
 });
-
-
-
 
 loadMoreBtn.addEventListener('click', async () => {
   currentPage += 1;
@@ -76,8 +75,12 @@ loadMoreBtn.addEventListener('click', async () => {
     const data = await getImagesByQuery(currentQuery, currentPage);
     const { hits } = data;
 
-    createGallery(hits);
-    refreshLightbox(); 
+    if (hits.length === 0) {
+      hideLoadMoreButton();
+      return;
+    }
+
+    createGallery(hits); 
 
     scrollPage();
 
@@ -85,14 +88,14 @@ loadMoreBtn.addEventListener('click', async () => {
       iziToast.info({
         message: "We're sorry, but you've reached the end of search results.",
       });
+      hideLoadMoreButton();
     } else {
       showLoadMoreButton();
     }
   } catch (error) {
     iziToast.error({ message: 'Failed to load more images.' });
+    hideLoadMoreButton();
   } finally {
     hideLoader();
   }
 });
-
-
